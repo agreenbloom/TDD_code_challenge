b4365a90e608e71dd1a90312743407c0261d1d4d
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-09-24 16:04:40 -0400

    check test suite working

--- test_pricing.rb
+++ test_pricing.rb
@@ -1,2 +1,13 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+
+require './pricing'
+
+class TestPricing < MiniTest::Test
+
+  # test base markup
+  def test_flat_markup_add_percentage
+
+  end
+
+end
