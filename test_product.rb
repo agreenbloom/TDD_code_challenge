1e4d568349b5fa7248c597f868e45aa285b910cc
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-09-24 17:14:14 -0400

    test pass markup three people

--- test_product.rb
+++ test_product.rb
@@ -18,4 +18,8 @@ class TestProduct < MiniTest::Test
   def test__person_markup_with_flat_markup_on_base_price
     assert_equal 1380.57, Product.new(1299.99).person_markup
   end
+
+  def test_three_people_with_markup
+    assert 1411.77, Product.new(1299.99).person_markup * 3
+  end
 end
