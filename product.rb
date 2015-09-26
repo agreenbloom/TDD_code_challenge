0fb2fc366b234def91de4c290052d67a6b9c476c
Abby Greenbloom <abby.greenbloom@gmail.com>
2015-09-26 17:16:54 -0400

    clean up code

--- product.rb
+++ product.rb
@@ -5,33 +5,27 @@ class Product
     @price = price
     @person = person
     @material = material
-
   end
 
 
   def flat_markup
     flat_markup = @price + (@price * 0.05)
-    # (flat_markup * 100).floor / 100.to_f
 
   end
 
   def person_markup
     @people = @person * 0.012
     person_markup = (flat_markup * @people)
-    # (person_markup * 100).floor / 100.to_f
   end
 
   def material_markup
     case @material
-     when 'pharmaceuticals'
+    when 'pharmaceuticals'
       pharm_markup = (flat_markup * 0.075)
-      # (pharm_markup * 100).floor / 100.to_f
-     when 'food'
+    when 'food'
       food_markup = (flat_markup * 0.13)
-      # (food_markup * 100).floor / 100.to_f
-     when 'electronics'
+    when 'electronics'
       electronic_markup = (flat_markup * 0.02)
-      # (electronic_markup * 100).floor / 100.to_f
     else 'other'
       0
     end
@@ -39,9 +33,6 @@ class Product
 
   def total
    total_price = (flat_markup + person_markup + material_markup).round(2)
-   # (total_price * 100).floor / 100.to_f
-
-
   end
 
 end
